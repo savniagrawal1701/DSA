39dadd2e284825b0fb3e3aaa28c636b774755f3b
Savni agrawal <savniagrawal794@gmail.com>
2025-11-17 19:21:46 +0530

    Add files via upload

---
+++ Binary search/ssearchinrotatedandsorted2.cpp
@@ -0,0 +1,50 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& arr, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+       
+        if (arr[mid] == k) return mid;
+if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+    low++;
+    high--;
+}
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                
+                high = mid - 1;
+            }
+            else {
+         
+                low = mid + 1;
+            }
+        }
+        else { 
+            if (arr[mid] <= k && k <= arr[high]) {
+              
+                low = mid + 1;
+            }
+            else {
+         
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {3,3,1,2,3,3,3,3,3};
+    int n = 9, k = 1;
+    bool ans = search(arr, n, k);
+    if (ans == false)
+        cout << "Target is not present.\n";
+    else
+        cout << "Target is present"<<" "<<endl;
+    return 0;
+}

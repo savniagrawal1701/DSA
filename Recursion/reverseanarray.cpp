1a013fe2bf9bf43dcf9dbb346664170f46a661d2
Savni agrawal <savniagrawal794@gmail.com>
2025-12-14 17:37:14 +0530

    Add files via upload

---
+++ Recursion/reverseanarray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+   
+    void reverseArray(vector<int>& arr) {
+        
+        int p1 = 0;
+        int p2 = arr.size() - 1;
+
+        while (p1 < p2) {
+          
+            swap(arr[p1], arr[p2]);
+
+            p1++;
+            p2--;
+        }
+    }
+};
+
+int main() {
+   
+    Solution sol;
+
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    sol.reverseArray(arr);
+
+    for (int num : arr) {
+        cout << num << " ";
+    }
+
+    return 0;
+}

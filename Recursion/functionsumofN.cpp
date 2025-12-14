4ab89206340a8445a50472c198612dbf43148e7c
Savni agrawal <savniagrawal794@gmail.com>
2025-12-14 11:17:35 +0530

    Add files via upload

---
+++ Recursion/functionsumofN.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+ 
+    int funcsum(int N) {
+        
+        if (N==0)
+            return 0;
+       return N+funcsum(N-1);
+    }
+};
+
+int main() {
+    Solution sol;
+ 
+    cout<<"The sum is:"<<sol.funcsum(3)<<endl;
+   
+    return 0;
+}

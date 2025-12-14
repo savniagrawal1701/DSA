e5a813097393729bbf347fa63e081c3af839d891
Savni agrawal <savniagrawal794@gmail.com>
2025-12-14 10:22:15 +0530

    Add files via upload

---
+++ Recursion/print1toN.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+ 
+    void print1toN( int count, int N) {
+        
+        if (count> N)
+            return;
+
+        cout << count << "\n";
+
+        print1toN( count + 1, N);
+    }
+};
+
+int main() {
+    Solution sol;
+    int N = 5;
+    
+
+    sol.print1toN( 1, N);
+
+    return 0;
+}

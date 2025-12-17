f6b4ba16b45db302d7c77dd3b2f07d9492c80d93
Savni agrawal <savniagrawal794@gmail.com>
2025-12-17 11:56:40 +0530

    Add files via upload

---
+++ Bit Manipulation/binarytodecimal.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int converttodecimal(string x)
+{
+    int len=x.length();
+    int power2=1;
+    int num=0;
+    for(int i =len-1;i>=0;i--){
+        if(x[i]=='1'){
+            num=num+power2;
+          
+        }
+          power2=power2*2;
+    }
+    return num;
+}
+int main(){
+    string n ;
+    cout<<"Entre the binary no "<<endl;
+    cin>>n;
+    
+    cout<<"The decimal of the given binary no is "<<converttodecimal(n);
+    return 0;
+}

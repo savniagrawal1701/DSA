9e6e61715d72794218d1f7b30535be7a5b8dc7be
Savni agrawal <savniagrawal794@gmail.com>
2025-12-19 12:42:33 +0530

    Add files via upload

---
+++ Bit Manipulation/XOR_from_LtoR.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int XORtillN(int n)
+    {
+        if (n % 4 == 1)
+            return 1;
+        if (n % 4 == 2)
+            return n + 1;
+        if (n % 4 == 3)
+            return 0;
+        return n;
+    }
+    int findRangeXOR(int l, int r)
+    {
+        return XORtillN(l - 1) ^ XORtillN(r);
+    }
+};
+int main()
+{
+    int l=4;
+    int r=7;
+    Solution sol;
+
+    int ans = sol.findRangeXOR(l,r);
+
+    cout << "The XOR of numbers from " << l<<"to " <<r<< " is: " << ans;
+
+    return 0;
+}

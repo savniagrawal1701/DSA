5f3bb7d64ba4e11ba270363d25ca9e215e3c1b13
Savni agrawal <savniagrawal794@gmail.com>
2025-12-25 16:39:16 +0530

    Add files via upload

---
+++ Stack/infix_to_prefix.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to return precedence of operators
+int prec(char c)
+{
+    if (c == '^') // Exponent operator has highest precedence
+        return 3;
+    else if (c == '/' || c == '*') // Multiplication and division have higher precedence than addition
+        return 2;
+    else if (c == '+' || c == '-') // Addition and subtraction have lowest precedence
+        return 1;
+    else
+        return -1;
+}
+
+void infixToPrefix(string s)
+{
+    stack<char> st;
+    string result;
+    reverse(s.begin(), s.end());
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == ')')
+            s[i] = '(';
+        else if (s[i] == '(')
+            s[i] = ')';
+    }
+    for (int i = 0; i < s.length(); i++)
+    {
+        char c = s[i];
+
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+            result += c;
+
+        else if (c == '(')
+            st.push('(');
+
+        else if (c == ')')
+        {
+            while (st.top() != '(')
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+
+        else
+        {
+            while (!st.empty() && prec(s[i]) < prec(st.top()))
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while (!st.empty())
+    {
+        result += st.top();
+        st.pop();
+    }
+    reverse(result.begin(),result.end());
+    cout << "Postfix expression: " << result << endl;
+}
+
+int main()
+{
+    string exp = "(A+B)*C-D+F";
+    cout << "Infix expression: " << exp << endl;
+    infixToPrefix(exp);
+    return 0;
+}

1c2f394c0a3828559a95653b71b82e02639bd98b
Savni agrawal <savniagrawal794@gmail.com>
2026-02-21 10:27:33 +0530

    Add files via upload

---
+++ Binary trees/maxipathsum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+class Solution {
+public:
+    
+    int maxPathSum(TreeNode* root) {
+        int maxSum = INT_MIN;
+        dfs(root, maxSum);
+        return maxSum;
+    }
+    int dfs(TreeNode* node, int &maxSum) {
+        if (!node) return 0;
+        int left = max(0, dfs(node->left, maxSum));
+        int right = max(0, dfs(node->right, maxSum));
+
+        maxSum = max(maxSum, left + right + node->val);
+
+        return max(left, right) + node->val;
+    }
+};
+int main() {
+    TreeNode* root = new TreeNode(-10);
+    root->left = new TreeNode(9);
+    root->right = new TreeNode(20);
+    root->right->left = new TreeNode(15);
+    root->right->right = new TreeNode(7);
+
+    Solution obj;
+    cout << "Maximum Path Sum: "
+         << obj.maxPathSum(root) << endl;
+
+    return 0;
+}

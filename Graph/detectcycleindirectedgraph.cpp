cc4f9439da05ef0d5e3d872b726c1a9d51cf9179
Savni agrawal <savniagrawal794@gmail.com>
2026-03-28 11:46:52 +0530

    Add files via upload

---
+++ Graph/detectcycleindirectedgraph.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[])
+    {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        for (auto it : adj[node])
+        {
+
+            if (!vis[it])
+            {
+                if (dfsCheck(it, adj, vis, pathVis) == true)
+                    return true;
+            }
+
+            else if (pathVis[it])
+            {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+public:
+    bool isCyclic(int V, vector<int> adj[])
+    {
+        int vis[V] = {0};
+        int pathVis[V] = {0};
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (dfsCheck(i, adj, vis, pathVis) == true)
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+
+    vector<int> adj[11] = {{}, {2}, {3}, {4, 7}, {5}, {6}, {}, {5}, {9}, {10}, {8}};
+    int V = 11;
+    Solution obj;
+    bool ans = obj.isCyclic(V, adj);
+
+    if (ans)
+        cout << "True\n";
+    else
+        cout << "False\n";
+
+    return 0;
+}

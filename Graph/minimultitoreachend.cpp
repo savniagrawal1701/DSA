3ee5062a22a4ed2bde4fabbde5e2045dc1f54948
Savni agrawal <savniagrawal794@gmail.com>
2026-05-02 16:38:17 +0530

    Add files via upload

---
+++ Graph/minimultitoreachend.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int minimumMultiplications(vector<int> &arr,
+                               int start, int end)
+    {
+
+        queue<pair<int, int>> q;
+        q.push({start, 0});
+
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        int mod = 100000;
+
+        while (!q.empty())
+        {
+            int node = q.front().first;
+            int steps = q.front().second;
+            q.pop();
+
+            for (auto it : arr)
+            {
+                int num = (it * node) % mod;
+
+                if (steps + 1 < dist[num])
+                {
+                    dist[num] = steps + 1;
+
+                    if (num == end)
+                        return steps + 1;
+                    q.push({num, steps + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main()
+{
+
+    int start = 3, end = 30;
+
+    vector<int> arr = {2, 5, 7};
+
+    Solution obj;
+
+    int ans = obj.minimumMultiplications(arr, start, end);
+
+    cout << ans;
+    cout << endl;
+
+    return 0;
+}
